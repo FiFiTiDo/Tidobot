8d3bc0b6be7a4eefa8179a4308c6941b82399636
Evan Fiordeliso <evan.fiordeliso@gmail.com>
2020-07-16 10:13:53 -0400

    Update general module

--- src/Modules/GeneralModule.ts
+++ src/Modules/GeneralModule.ts
@@ -1,14 +1,11 @@
-import AbstractModule, {ModuleInfo, Symbols, Systems} from "./AbstractModule";
+import AbstractModule, {Symbols} from "./AbstractModule";
 import moment from "moment-timezone";
-import {array_rand, tuple} from "../Utilities/ArrayUtils";
+import {array_rand} from "../Utilities/ArrayUtils";
 import Permission from "../Systems/Permissions/Permission";
 import {Role} from "../Systems/Permissions/Role";
 import Setting, {SettingType} from "../Systems/Settings/Setting";
 import Command from "../Systems/Commands/Command";
-import {CommandEventArgs} from "../Systems/Commands/CommandEvent";
-import {string} from "../Systems/Commands/Validation/String";
-import StandardValidationStrategy from "../Systems/Commands/Validation/Strategies/StandardValidationStrategy";
-import {ValidatorStatus} from "../Systems/Commands/Validation/Strategies/ValidationStrategy";
+import {CommandEvent} from "../Systems/Commands/CommandEvent";
 import Bot from "../Application/Bot";
 import {command} from "../Systems/Commands/decorators";
 import Message from "../Chat/Message";
@@ -16,103 +13,78 @@ import {ExpressionContext} from "../Systems/Expressions/ExpressionSystem";
 import {ExpressionContextResolver} from "../Systems/Expressions/decorators";
 import {permission} from "../Systems/Permissions/decorators";
 import {setting} from "../Systems/Settings/decorators";
-import {MomentZone} from "moment-timezone/moment-timezone";
+import {CommandHandler} from "../Systems/Commands/Validation/CommandHandler";
+import CheckPermission from "../Systems/Commands/Validation/CheckPermission";
+import {MessageArg, ResponseArg, RestArguments, Sender} from "../Systems/Commands/Validation/Argument";
+import {Response} from "../Chat/Response";
+import ChatterEntity from "../Database/Entities/ChatterEntity";
 
 export const MODULE_INFO = {
     name: "General",
-    version: "1.0.2",
+    version: "1.1.0",
     description: "General bot commands that don't fit in other modules"
 };
 
 class PingCommand extends Command {
-    constructor(private generalModule: GeneralModule) {
+    constructor() {
         super("ping", null);
     }
 
-    async execute({event, message: msg, response}: CommandEventArgs): Promise<void> {
-        const {status} = await event.validate(new StandardValidationStrategy({
-            usage: "ping",
-            permission: this.generalModule.ping
-        }));
-         if (status !== ValidatorStatus.OK) return;
-
-        await response.message("pong", {username: msg.getChatter().name});
+    @CommandHandler("ping", "ping")
+    @CheckPermission("general.ping")
+    async handleCommand(event: CommandEvent, @ResponseArg response: Response, @Sender sender: ChatterEntity): Promise<void> {
+        return response.message("pong", {username: sender.name});
     }
 }
 
 class RawCommand extends Command {
-    constructor(private generalModule: GeneralModule) {
+    constructor() {
         super("raw", "<text>");
     }
 
-    async execute({event, response}: CommandEventArgs): Promise<void> {
-        const {args, status} = await event.validate(new StandardValidationStrategy({
-            usage: "raw <text>",
-            arguments: tuple(
-                string({ name: "text", required: true, greedy: true })
-            ),
-            permission: this.generalModule.raw
-        }));
-         if (status !== ValidatorStatus.OK) return;
-        const [value] = args;
-
-        await response.rawMessage(value);
+    @CommandHandler("raw", "raw <text>")
+    @CheckPermission("general.raw")
+    async handleCommand(event: CommandEvent, @ResponseArg response: Response, @RestArguments(true, true) text: string): Promise<void> {
+        return response.rawMessage(text);
     }
 }
 
 class EchoCommand extends Command {
-    constructor(private generalModule: GeneralModule) {
+    constructor() {
         super("echo", "<message>");
     }
 
-    async execute({event, response}: CommandEventArgs): Promise<void> {
-        const {args, status} = await event.validate(new StandardValidationStrategy({
-            usage: "echo <message>",
-            arguments: tuple(
-                string({ name: "message", required: true, greedy: true })
-            ),
-            permission: this.generalModule.echo
-        }));
-         if (status !== ValidatorStatus.OK) return;
-        const [message] = args;
-
-        await response.rawMessage(">> " + message);
+    @CommandHandler("echo", "echo <message>")
+    @CheckPermission("general.echo")
+    async handleCommand(event: CommandEvent, @ResponseArg response: Response, @RestArguments(true, true) message: string): Promise<void> {
+        return  response.rawMessage(">> " + message);
     }
 }
 
 class EvalCommand extends Command {
-    constructor(private generalModule: GeneralModule) {
+    constructor() {
         super("eval", "<expression>");
     }
 
-    async execute({event, message: msg, response}: CommandEventArgs): Promise<void> {
-        const {args, status} = await event.validate(new StandardValidationStrategy({
-            usage: "eval <expression>",
-            arguments: tuple(
-                string({ name: "expression", required: true, greedy: true })
-            ),
-            permission: this.generalModule.eval
-        }));
-         if (status !== ValidatorStatus.OK) return;
-        const [rawExpr] = args;
-
+    @CommandHandler("eval", "eval <expression>")
+    @CheckPermission("general.eval")
+    async handleCommand(
+        event: CommandEvent, @ResponseArg response: Response, @MessageArg msg: Message,
+        @RestArguments(true, true) rawExpr: string
+    ): Promise<void> {
         await response.rawMessage(">> " + await msg.evaluateExpression(rawExpr));
     }
 }
 
 class ShutdownCommand extends Command {
-    constructor(private generalModule: GeneralModule) {
+    constructor() {
         super("shutdown", null);
     }
 
-    async execute({event, response}: CommandEventArgs): Promise<void> {
-        const {status} = await event.validate(new StandardValidationStrategy({
-            usage: "shutdown",
-            permission: this.generalModule.shutdown
-        }));
-         if (status !== ValidatorStatus.OK) return;
-
-        await response.broadcast(array_rand(await response.getTranslation("shutdown")));
+    @CommandHandler("shutdown", "shutdown")
+    @CheckPermission("general.shutdown")
+    async handleCommand(event: CommandEvent, @ResponseArg response: Response): Promise<void> {
+        await response.broadcast(array_rand(await response.getTranslation<string[]>("shutdown")));
         Bot.LOGGER.info("Shutting down...");
         process.exit();
     }
@@ -127,16 +99,16 @@ export default class GeneralModule extends AbstractModule {
         this.coreModule = true;
     }
 
-    @command pingCommand = new PingCommand(this);
-    @command rawCommand = new RawCommand(this);
-    @command echoCommand = new EchoCommand(this);
-    @command evalCommand = new EvalCommand(this);
-    @command shutdownCommand = new ShutdownCommand(this);
+    @command pingCommand = new PingCommand();
+    @command rawCommand = new RawCommand();
+    @command echoCommand = new EchoCommand();
+    @command evalCommand = new EvalCommand();
+    @command shutdownCommand = new ShutdownCommand();
 
     @permission ping = new Permission("general.ping", Role.MODERATOR);
     @permission raw = new Permission("general.raw", Role.OWNER);
     @permission echo = new Permission("general.echo", Role.MODERATOR);
-    @permission eval = new Permission("general.eval", Role.MODERATOR);
+    @permission evalPerm = new Permission("general.eval", Role.MODERATOR);
     @permission shutdown = new Permission("general.shutdown", Role.OWNER);
 
     @setting timezone = new Setting("timezone", moment.tz.zone("America/New_York"), SettingType.TIMEZONE);
